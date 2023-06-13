f953e93cd79c455f1cb12ad8220f1eb8476847a6
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-06-13 23:21:27 +0900

    regroupまで学習完了

--- quick_django/main/urls.py
+++ quick_django/main/urls.py
@@ -6,4 +6,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('temp', views.temp, name='temp'),
     path('list', views.list, name='list'),
+    path('iftag', views.iftag, name='iftag'),
+    path('yesno', views.yesno, name='yesno'),
+    path('firstof', views.firstof, name='firstof'),
+    path('forloop', views.forloop, name='forloop'),
+    path('forempty', views.forempty, name='forempty'),
+    path('fortag', views.fortag, name='fortag'),
+    path('ifchanged', views.ifchanged, name='ifchanged'),
+    path('regroup', views.regroup, name='regroup'),
 ]
--- quick_django/main/views.py
+++ quick_django/main/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
+import random
 
 def index(request):
     return HttpResponse('こんにちは、世界！')
@@ -18,4 +19,56 @@ def list(request):
     books = Book.objects.all()
     return render(request, 'main/list.html', {
         'books': books
+    })
+
+def iftag(request):
+    return render(request, 'main/iftag.html', {
+        'random': random.randint(0, 100)
+    })
+    
+def yesno(request):
+    return render(request, 'main/yesno.html', {
+        'flag': True
+    })
+
+def firstof(request):
+    return render(request, 'main/firstof.html', {
+        #'a': 'おはようございます。',
+        #'b': 'こんにちは',
+        #'c': 'こんばんは',
+    })
+
+def forloop(request):
+    return render(request, 'main/forloop.html', {
+        'weeks': ['月','火','水','木','金','土','日']
+    })
+
+def forempty(request):
+    return render(request, 'main/forempty.html', {
+        # 'members': ['鈴木三郎', '佐藤洋子', '山田次郎']
+        'members': []
+    })
+    
+def fortag(request):
+    return render(request, 'main/fortag.html')
+
+def ifchanged(request):
+    return render(request, 'main/ifchanged.html', {
+        'schedule': [
+            (10, 'A企画反省会'),
+            (10, 'B書籍脱稿'),
+            (15, 'WINGS定例会議'),
+            (30, 'C企画打ち合わせ')
+        ]
+    })
+    
+def regroup(request):
+    return render(request, 'main/regroup.html', {
+        'members': [
+            {'name': '鈴木三郎', 'sex': '男', 'birth': '1980-12-23'},
+            {'name': '山田次郎', 'sex': '男', 'birth': '1978-10-13'},
+            {'name': '佐藤健司', 'sex': '男', 'birth': '1976-04-06'},
+            {'name': '山本花子', 'sex': '女', 'birth': '1981-07-28'},
+            {'name': '田中久美', 'sex': '女', 'birth': '1980-09-07'},
+        ]
     })
